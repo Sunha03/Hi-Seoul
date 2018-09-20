2fb841f927cdf9ac32754cdf78a942aa6da6a245
Sunha03 <ahnus123@gmail.com>
2018-09-21 01:32:23 +0900

    Connecting MainActivity and WeatherActivity

--- app/src/main/java/com/hs/hi_seoul/MainActivity.java
+++ app/src/main/java/com/hs/hi_seoul/MainActivity.java
@@ -1,7 +1,9 @@
 package com.hs.hi_seoul;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v) {
+        if(v.getId() == R.id.btn_start) {
+            Intent intent = new Intent(this, WeatherActivity.class);
+            startActivity(intent);
+        }
     }
 }
