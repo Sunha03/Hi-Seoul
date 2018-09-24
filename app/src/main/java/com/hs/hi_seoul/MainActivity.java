aa393b1db4bea94ce0279ea44db3351ea0cc58ef
Sunha03 <ahnus123@gmail.com>
2018-09-24 21:02:05 +0900

    Get Android UniqueId

--- app/src/main/java/com/hs/hi_seoul/MainActivity.java
+++ app/src/main/java/com/hs/hi_seoul/MainActivity.java
@@ -1,8 +1,10 @@
 package com.hs.hi_seoul;
 
 import android.content.Intent;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String idByANDROID_ID = Settings.Secure.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID);
+        Log.e("superdroid", "============ID : " + idByANDROID_ID);
     }
 
     public void onClick(View v) {
