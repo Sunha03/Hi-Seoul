b41ffd51606d3bb942537a5abe25c3caa9a1563e
Sunha03 <ahnus123@gmail.com>
2018-09-28 01:46:57 +0900

    Add LineActivity

---
+++ app/src/main/java/com/hs/hi_seoul/LineActivity.java
@@ -0,0 +1,13 @@
+package com.hs.hi_seoul;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_line);
+    }
+}
